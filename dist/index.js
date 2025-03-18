ae878c3ff4746077fab7eae5466e32eaff1b7b2b
Holenko M.O. <m.o.holenko@student.khai.edu>
2025-03-19 01:24:10 +0200

    server/init commit

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const utils_1 = require("#core/utils");
-const test = "This should trigger an ESLint error"; // Ошибка ESLint
-async function testFunction() {
-    return true;
-}
-testFunction(); // Ошибка ESLint (вызов без await)
-console.log((0, utils_1.getMessage)());
